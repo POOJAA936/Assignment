6051f509fe80df6d00d4b4fda7b679f673a11b93
Hi <Hi@DESKTOP-C1282G7>
2020-06-22 23:38:10 -0700

    Add

--- src/DropDown/GetallUsers.java
+++ src/DropDown/GetallUsers.java
@@ -1,22 +1,27 @@
 package DropDown;
 import org.testng.annotations.Test;
 import io.restassured.http.ContentType;
+import io.restassured.internal.util.IOUtils;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 public class GetallUsers 
 {
 	@Test
-	public void demo1()
+	public void demo1() throws IOException
 	{
+		FileInputStream fin=new FileInputStream("D:\\MyWorkSpace\\Assignment\\employee.json");
 		given()
 		.when()
-		.get("http://dummy.restapiexample.com/api/v1/employees")
+		.contentType(ContentType.JSON)
+		.body(IOUtils.toByteArray(fin))
+		.put("http://dummy.restapiexample.com/api/v1/update/21")
 		.then()
 		.assertThat()
 		.and()
-		.assertThat().statusCode(200)
+		.assertThat().statusCode(200).log().all()
 		.and()
-		.body("data", hasSize(25))
-		.header("content-length", equalTo("595"));
+		.header("content-length", equalTo("32")).log().all();
 	}
 }
